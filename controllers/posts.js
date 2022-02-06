77608e76d072e55b9cd20eafa930e3dd399a1904
tamaraandrea <tamara18_1985@msn.com>
2022-02-05 23:43:55 -0500

    connected to the db

---
+++ controllers/posts.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const Posts = require('../models/posts')
+
+//Index Route
+
+//Create/Post Route
+
+//Show Route
+
+//Delete Route
+
+//Update Route
+
+
+module.exports = router
